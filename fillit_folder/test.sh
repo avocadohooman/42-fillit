f14f5fad7d9582fd9970d18162c0a5012e282545
Gerhard Molin <gerhard@minimumbadass.com>
2019-11-16 21:45:37 +0200

    updated test executable

--- fillit_folder/test.sh
+++ fillit_folder/test.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
 ./a.out ../tetris_blocks/valid_4t_subject_example.txt
-
+echo ----4T Subject Example Test---
+./a.out ../tetris_blocks/valid_6t.txt
+echo ----6T Test----
+./a.out ../tetris_blocks/valid_hard_7t.txt
+echo ----HARD 7T Test----
+./a.out ../tetris_blocks/valid_8t.txt
+echo ----8T Subject Example Test----
