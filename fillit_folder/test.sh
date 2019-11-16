a91c05e536e0d2dfa23b5aeafa6607123aeb0f04
Gerhard Molin <gerhard@minimumbadass.com>
2019-11-16 22:02:43 +0200

    creates test.sh which tests various amounts of tetris blocks and compars it with a working fillit

--- fillit_folder/test.sh
+++ fillit_folder/test.sh
@@ -1,9 +1,33 @@
 #!/bin/bash
-./a.out ../tetris_blocks/valid_4t_subject_example.txt
+echo ----1T Test---
+./a.out ../tetris_blocks/valid_1t_c.txt
+echo ----1T Comparison Test---
+./fillit ../tetris_blocks/valid_1t_c.txt
+echo ----2T Test---
+./a.out ../tetris_blocks/valid_2t_a.txt
+echo ----2T Comparison Test---
+./fillit ../tetris_blocks/valid_2t_a.txt
+echo ----3T Comparison Test---
+./fillit ../tetris_blocks/valid_3t_c.txt
+echo ----3T Test---
+./a.out ../tetris_blocks/valid_3t_c.txt
 echo ----4T Subject Example Test---
-./a.out ../tetris_blocks/valid_6t.txt
+./a.out ../tetris_blocks/valid_4t_subject_example.txt
+echo ----4T Subject Comparison Test---
+./fillit ../tetris_blocks/valid_4t_subject_example.txt
 echo ----6T Test----
-./a.out ../tetris_blocks/valid_hard_7t.txt
+./a.out ../tetris_blocks/valid_6t.txt
+echo ----6T Comparison Test----
+./fillit ../tetris_blocks/valid_6t.txt
 echo ----HARD 7T Test----
-./a.out ../tetris_blocks/valid_8t.txt
+./a.out ../tetris_blocks/valid_hard_7t.txt
+echo ----HARD 7T Comparison----
+./fillit ../tetris_blocks/valid_hard_7t.txt
 echo ----8T Subject Example Test----
+./a.out ../tetris_blocks/valid_8t.txt
+echo ----8T Subject Comparison----
+./fillit ../tetris_blocks/valid_8t.txt
+echo ----8T Hard Test----
+./a.out ../tetris_blocks/valid_hard_8t.txt
+echo ----8T Hard Comparison Test----
+./fillit ../tetris_blocks/valid_hard_8t.txt
